144837d9735dfd0aac47eb33ae15ab0632947c4d
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-22 21:49:14 +0800

    hello

---
+++ hello-world/pages/index/index.vue
@@ -0,0 +1,55 @@
+<template>
+	<view class="content">
+		<button @click="onClick">hello</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {},
+		methods: {
+			async onClick() {
+				const col = uniCloud.importObject("col")
+				const res = await col.say('st')
+				console.log(res);
+				uni.showModal({
+					content: JSON.stringify(res),
+					showCancel: false
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
