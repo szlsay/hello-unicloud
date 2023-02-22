f10798d61f1867d908d45488860840296fc01a13
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-22 21:03:40 +0800

    hello

--- HelloUniCloud/pages/index/index.vue
+++ HelloUniCloud/pages/index/index.vue
@@ -1,17 +1,18 @@
 <template>
 	<view class="content">
+		<uni-icons type="contact" size="30"></uni-icons>
 		<!-- 		<image class="logo" src="/static/logo.png"></image>
 		<view class="text-area">
 			<text class="title">{{title}}</text>
 		</view> -->
-		<!-- 	<view v-for="item in users" :key="item._id">
-			<view>姓名：{{ item.name }}</view>
-		</view> -->
-		<form @submit="onSubmit">
+		<view class="out" v-for="item in users" :key="item._id">
+			<view class="row">姓名：{{ item.name }}</view>
+		</view>
+		<!-- 	<form @submit="onSubmit">
 			<input type="text" name="name">
 			<input type="number" name="age">
 			<button form-type="submit">提交</button>
-		</form>
+		</form> -->
 	</view>
 </template>
 
@@ -26,8 +27,17 @@
 		onLoad() {
 			// this.loadHello()
 			// this.loadUser()
+			// this.getUser()
 		},
 		methods: {
+			getUser() {
+				uniCloud.callFunction({
+					name: "getuser"
+				}).then(res => {
+					console.log(res)
+					this.users = res.result.data
+				})
+			},
 			onSubmit(event) {
 				let data = event.detail.value
 				console.log("提交数据：", data);
@@ -64,30 +74,39 @@
 	}
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+<style lang="scss">
+	.out {
+		padding: 30rpx;
+
+		.row {
+			border-bottom: 1px solid #ccc;
+			padding: 20rpx 0;
+		}
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
+	// .content {
+	// 	display: flex;
+	// 	flex-direction: column;
+	// 	align-items: center;
+	// 	justify-content: center;
+	// }
 
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
+	// .logo {
+	// 	height: 200rpx;
+	// 	width: 200rpx;
+	// 	margin-top: 200rpx;
+	// 	margin-left: auto;
+	// 	margin-right: auto;
+	// 	margin-bottom: 50rpx;
+	// }
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+	// .text-area {
+	// 	display: flex;
+	// 	justify-content: center;
+	// }
+
+	// .title {
+	// 	font-size: 36rpx;
+	// 	color: #8f8f94;
+	// }
 </style>
