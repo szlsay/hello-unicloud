9a26ac32c9b855cbe14041284afa876f3ea6ee40
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-22 21:57:08 +0800

    通讯录

---
+++ 通讯录/pages/list/list.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<unicloud-db v-slot:default="{data, loading, error, options}" collection="contacts">
+			<view v-if="error">{{error.message}}</view>
+			<view v-else>
+				{{data}}
+			</view>
+		</unicloud-db>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
