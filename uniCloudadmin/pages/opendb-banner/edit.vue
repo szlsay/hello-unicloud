95b11b3fa8b9e1bd566fea3d338c08514a6c30ee
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-02-25 21:29:02 +0800

    admin

--- uniCloudadmin/pages/opendb-banner/edit.vue
+++ uniCloudadmin/pages/opendb-banner/edit.vue
@@ -1,6 +1,6 @@
 <template>
   <view class="uni-container">
-    <uni-forms ref="form" :model="formData" validate-trigger="submit" err-show-type="toast">
+    <uni-forms ref="form" :model="formData" validateTrigger="bind">
       <uni-forms-item name="bannerfile" label="图片文件" required>
         <uni-file-picker file-mediatype="image" return-type="object" v-model="formData.bannerfile"></uni-file-picker>
       </uni-forms-item>
@@ -23,7 +23,10 @@
         <uni-easyinput placeholder="维护者自用描述" v-model="formData.description" trim="both"></uni-easyinput>
       </uni-forms-item>
       <view class="uni-button-group">
-        <button type="primary" class="uni-button" @click="submit">提交</button>
+        <button type="primary" class="uni-button" style="width: 100px;" @click="submit">提交</button>
+        <navigator open-type="navigateBack" style="margin-left: 15px;">
+          <button class="uni-button" style="width: 100px;">返回</button>
+        </navigator>
       </view>
     </uni-forms>
   </view>
@@ -33,12 +36,13 @@
   import { validator } from '../../js_sdk/validator/opendb-banner.js';
 
   const db = uniCloud.database();
+  const dbCmd = db.command;
   const dbCollectionName = 'opendb-banner';
 
   function getValidator(fields) {
     let result = {}
     for (let key in validator) {
-      if (fields.indexOf(key) > -1) {
+      if (fields.includes(key)) {
         result[key] = validator[key]
       }
     }
@@ -100,7 +104,6 @@
         // 使用 clientDB 提交数据
         return db.collection(dbCollectionName).doc(this.formDataId).update(value).then((res) => {
           uni.showToast({
-            icon: 'none',
             title: '修改成功'
           })
           this.getOpenerEventChannel().emit('refreshData')
@@ -139,42 +142,3 @@
     }
   }
 </script>
-
-<style>
-  .uni-container {
-    padding: 15px;
-  }
-
-  .uni-input-border,
-  .uni-textarea-border {
-    width: 100%;
-    font-size: 14px;
-    color: #666;
-    border: 1px #e5e5e5 solid;
-    border-radius: 5px;
-    box-sizing: border-box;
-  }
-
-  .uni-input-border {
-    padding: 0 10px;
-    height: 35px;
-
-  }
-
-  .uni-textarea-border {
-    padding: 10px;
-    height: 80px;
-  }
-
-  .uni-button-group {
-    margin-top: 50px;
-    /* #ifndef APP-NVUE */
-    display: flex;
-    /* #endif */
-    justify-content: center;
-  }
-
-  .uni-button {
-    width: 184px;
-  }
-</style>
